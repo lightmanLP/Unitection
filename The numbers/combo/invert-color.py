8c1b3c8c388a8d1ad2c05056193d7bdeb55594c4
lightmanLP <liteman1000@gmail.com>
2019-10-18 18:07:47 +0700

    some new and old

---
+++ The numbers/combo/invert-color.py
@@ -0,0 +1,16 @@
+from os import listdir as LD
+from PIL import Image, ImageOps
+
+for i in LD():
+ try:
+  if not(i.endswith('.png')):
+   continue
+  image = Image.open(i)
+  if image.mode == 'RGBA':
+   r,g,b,a = image.split()
+   Image.merge('RGBA',list(ImageOps.invert(Image.merge('RGB', (r,g,b))).split())+[a]).save(i)
+  else:
+   ImageOps.invert(image).save(i)
+  print(i)
+ except:
+  print('ERR: '+i)

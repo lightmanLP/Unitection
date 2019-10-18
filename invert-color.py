7bf65fa78f946c65212ecafca4e9a091bb01739a
lightmanLP <liteman1000@gmail.com>
2019-10-18 17:13:54 +0700

    fix invert

--- invert-color.py
+++ invert-color.py
@@ -2,12 +2,15 @@ from os import listdir as LD
 from PIL import Image, ImageOps
 
 for i in LD():
- if not(i.endswith('.png')):
-  continue
- image = Image.open(i)
- if image.mode == 'RGBA':
-  r,g,b,a = image.split()
-  Image.merge('RGBA',list(ImageOps.invert(Image.merge('RGB', (r,g,b))).split())+[a]).save(i)
- else:
-  ImageOps.invert(image).save(i)
- print(i)
+ try:
+  if not(i.endswith('.png')):
+   continue
+  image = Image.open(i)
+  if image.mode == 'RGBA':
+   r,g,b,a = image.split()
+   Image.merge('RGBA',list(ImageOps.invert(Image.merge('RGB', (r,g,b))).split())+[a]).save(i)
+  else:
+   ImageOps.invert(image).save(i)
+  print(i)
+ except:
+  print('ERR: '+i)
